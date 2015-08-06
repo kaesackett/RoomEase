6ee50822f375d3793a55f5b91cf166f8e3349687
kaesackett <kaesackett@yahoo.com>
2015-08-06 16:48:37 -0700

    updated secrets.sh

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 from model import User, Bill, House, User_Payment
-import datetime
 from flask import session
+import datetime
+import os
+
+my_number = os.environ['my_number']
 
 def send_text_reminder(email, client):
     """Check all a house's bills for upcoming due dates. 
@@ -15,7 +18,7 @@ def send_text_reminder(email, client):
         residents_who_havent_paid = find_bill_users_who_havent_paid(bill.bill_id)
         for resident in residents_who_havent_paid:
             if bill.due_date == datetime.date.today() + datetime.timedelta(days=1):
-                message = client.messages.create(to="+15203857877", from_="+12568874445", body="Heads up: Your "+str(bill.description)+" bill is due tomorrow!")
+                message = client.messages.create(to=my_number, from_="+12568874445", body="Heads up: Your "+str(bill.description)+" bill is due tomorrow!")
 
 def find_bill_users_who_havent_paid(bill_id):
     user_payments_for_bill = User_Payment.query.filter_by(bill_id=bill_id, paid=False).all()
